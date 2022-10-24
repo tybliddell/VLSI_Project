c1bfa01d37078308b66485aa78708d06672bc4dd
Braxton Chappell <92498909+StarchyBagel@users.noreply.github.com>
2022-10-24 15:34:50 -0600

    Added more details

--- cache_coherenter.v
+++ cache_coherenter.v
@@ -23,14 +23,15 @@
 //Just added all the macros because probably will use
 
 module cache_coherenter(
+    input clock, reset,
     input [32:0] cache_change,
     /* We should have a bit to see which cahce it is
     * coming from. Or we could have two inputs and
     * separate them by left cahce and right cache.
     */
-    input [32:0] left_cache_change,
-    input [32:0] right_cache_change,
-    output [32:0] cache_write
+    input [32:0] left_cache_change, right_cache_change
+
+    output [32:0] right_cache_write, left_cache_write
     /* Another thought: in our cache statemachine, 
     * we should have a state that takes an input from
     * the coherenter that has immediate priority. 
@@ -39,3 +40,21 @@ module cache_coherenter(
     */
 
 );
+
+//Something like below. THIS WILL CHANGE.
+//Probably need an idle so once the output is sent, it goes back to idle
+
+parameter idle = 3'b000;
+
+always @(right_cache_change) begin
+    //Send STUFF
+    left_cache_write <= right_cache_change;
+end
+
+always @(left_cache_change) begin
+    //Send STUFF
+    right_cache_write <= left_cache_change;
+end
+
+
+endmodule
