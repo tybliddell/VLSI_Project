d93e5c768ed200e6e6918060bc2f25acfd86c01d
Tyler Liddell <tybliddell@gmail.com>
2022-10-26 11:08:12 -0600

    Change to single byte writing. Added sim_memory to tb.

--- cache.v
+++ cache.v
@@ -8,7 +8,8 @@
 `define READ 1'd0
 `define WRITE 1'd1
 
-`define CPU_REQUEST_DATA 31:16
+`define CPU_REQUEST_COMMAND 24
+`define CPU_REQUEST_DATA 23:16
 `define CPU_REQUEST_ADDRESS 15:0
 `define CPU_REQUEST_TAG 15:8
 `define CPU_REQUEST_INDEX 7:1
@@ -21,14 +22,23 @@
 `define MEMORY_RESPONSE_UPPER 15:8
 `define MEMORY_RESPONSE_LOWER 7:0
 
+/* We send a memory request for a specific address.
+*  The memory response is an entire 16 bits so we can fill the cache entry.
+*  This is so we can actually know if an entry is valid
+*  (wouldn't work if we set half without knowing the other half was set).
+*/
 module cache(
-    input [32:0] cpu_request, /* 1 bit for r/w, 16 bits for data, 16 bits for address */
+    /* Should writes only write 8 bits of data? Since we are doing byte addressable reads
+    *  it doesn't make sense to do a write of 16 bits to an odd address- shouldn't remove the data before/after that spot
+    *  => cpu_request should become 1 + 8 + 16 bits wide! Same for memory request
+    */
+    input [24:0] cpu_request, /* 1 bit for r/w, 8 bits for data, 16 bits for address */
     input cpu_request_ready, /* cpu_request is populated, ready for processing */
     input clock, input reset,
     input [15:0] invalidate_address, /* coming from controller, invalidate that entry */
     input [15:0] memory_response, /* 16 bits for data */
     input memory_response_ready,
-    output reg [32:0] memory_request, /* 1 bit for r/w, 16 bits for data, 16 bits for address */
+    output reg [24:0] memory_request, /* 1 bit for r/w, 8 bits for data, 16 bits for address */
     output reg memory_request_ready, /* memory_request is populated, ready for processing */
     output reg [7:0] data_out, /* return data to cpu */
     output reg data_out_ready /* return data to cpu is ready */
@@ -65,7 +75,7 @@ module cache(
                 for(i = 0; i < `CACHE_BLOCK_COUNT; i = i + 1) begin
                     cache_mem[i][`CACHE_MEM_VALIDITY] <= 1'd0;
                 end
-                memory_request <= 33'd0;
+                memory_request <= 25'd0;
                 memory_request_ready <= 1'd0;
                 data_out <= 8'd0;
                 data_out_ready <= 1'd0;
@@ -76,30 +86,30 @@ module cache(
                     next_state <= COMPARE_TAG;
                 else
                     next_state <= state == IDLE ? _IDLE : IDLE;
-                memory_request <= 33'd0;
+                memory_request <= 25'd0;
                 memory_request_ready <= 1'd0;
                 data_out <= data_out;
                 data_out_ready <= 1'd0;
             end
             COMPARE_TAG: begin
                 // Read
-                if(cpu_request[32] == 1'b0) begin
+                if(cpu_request[`CPU_REQUEST_COMMAND] == 1'b0) begin
                     // cache_mem[][]
                     // if cache entry tag matches cpu_request tag and is valid
                     if(cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_TAG] == cpu_request[`CPU_REQUEST_TAG]
                         && cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_VALIDITY] == 1'b1) begin
-                        memory_request <= 33'd0;
+                        memory_request <= 25'd0;
                         memory_request_ready <= 1'd0;
                         if(cpu_request[`CPU_REQUEST_OFFSET] == 1'b0)
-                            data_out <= cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_DATA_UPPER];
-                        else
                             data_out <= cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_DATA_LOWER];
+                        else
+                            data_out <= cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_DATA_UPPER];
                         data_out_ready <= 1'd1;
                         next_state <= IDLE;
                     end
                     // cache entry is not present or valid
                     else begin //Miss
-                        memory_request <= { `READ, 16'd0, cpu_request[`CPU_REQUEST_ADDRESS] };
+                        memory_request <= { `READ, 8'd0, cpu_request[`CPU_REQUEST_ADDRESS] };
                         memory_request_ready <= 1'd1;
                         data_out <= data_out;
                         data_out_ready <= 1'd0;
@@ -110,8 +120,11 @@ module cache(
                 else begin
                     // Write to cache, and send a memory request
                     cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_TAG] <= cpu_request[`CPU_REQUEST_TAG];
-                    cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_DATA] <= cpu_request[`CPU_REQUEST_DATA];
                     cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_VALIDITY] <= 1'd1;
+                    if(cpu_request[`CPU_REQUEST_OFFSET] == 1'b0)
+                        cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_DATA_LOWER] <= cpu_request[`CPU_REQUEST_DATA];
+                    else
+                        cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_DATA_UPPER] <= cpu_request[`CPU_REQUEST_DATA];
                     memory_request <= { `WRITE, cpu_request[`CPU_REQUEST_DATA], cpu_request[`CPU_REQUEST_ADDRESS] };
                     memory_request_ready <= 1'd1;
                     data_out <= data_out;
@@ -124,12 +137,12 @@ module cache(
                     //write memory_response to cache
                     cache_mem[cpu_request[`CPU_REQUEST_INDEX]][`CACHE_MEM_DATA] <= memory_response;
                     data_out_ready <= 1'd1;
-                    memory_request <= 33'd0;
+                    memory_request <= 25'd0;
                     memory_request_ready <= 1'd0;
                     if(cpu_request[`CPU_REQUEST_OFFSET] == 1'b0)
-                        data_out <= memory_response[`MEMORY_RESPONSE_UPPER];
-                    else
                         data_out <= memory_response[`MEMORY_RESPONSE_LOWER];
+                    else
+                        data_out <= memory_response[`MEMORY_RESPONSE_UPPER];
                     next_state <= IDLE;
                 end
                 else begin
--- cache_tb.v
+++ cache_tb.v
@@ -4,11 +4,13 @@ module cache_tb;
     localparam period = 20;
     localparam half_period = period / 2;
 
-    reg[32:0] cpu_request;
+    reg[65_534 * 8:0] sim_memory;
+
+    reg[24:0] cpu_request;
     reg[15:0] invalidate_address, memory_response;
     reg clock, reset, cpu_request_ready, memory_response_ready;
 
-    wire [32:0] memory_request;
+    wire [24:0] memory_request;
     wire [7:0] data_out;
     wire data_out_ready, memory_request_ready;
 
@@ -20,16 +22,17 @@ module cache_tb;
         clock = ~clock;
     end
 
+    /* reset the cache to all invalid, inputs to 0, and sim_memory to 0 */
     task reset_cache;
         begin
             clock = 1'b0;
             reset = 1'b1;
             cpu_request_ready = 1'd0;
             memory_response_ready = 1'd0;
-            cpu_request = 33'd0;
+            cpu_request = 25'd0;
             invalidate_address = 16'd0;
             memory_response = 16'd0;
-
+            sim_memory = 65_535'd0;
             #period;
             reset = 1'd0;
             #period;
@@ -39,9 +42,12 @@ module cache_tb;
         end
     endtask
 
+    /* Write data to address. */
     task write_value;
-        input [15:0] data, address;
+        input [7:0] data;
+        input [15:0] address;
         begin
+            #period;
             cpu_request = { 1'd1, data, address };
             cpu_request_ready = 1'd1;
 
@@ -50,7 +56,13 @@ module cache_tb;
             end
             // simulate memory
             if(memory_request_ready == 1'd1) begin
-                memory_response = data;
+                sim_memory[address * 8+:8] = data;
+                // starting at address * 8 bits going to address * 8 + 16 bits
+                if(address[0] == 1'b0)
+                    memory_response = sim_memory[address * 8+:16];
+                // starting at (address - 1) * 8 bits going to (address - 1) * 8 + 16 bits
+                else
+                    memory_response = sim_memory[(address - 1) * 8+:16];
                 memory_response_ready = 1'd1;
                 // wait for data
                 while(data_out_ready == 1'd0) begin
@@ -58,24 +70,31 @@ module cache_tb;
                 end
             end
 
-            cpu_request = 33'd0;
+            cpu_request = 25'd0;
             cpu_request_ready = 1'd0;
             memory_response = 16'd0;
             memory_response_ready = 1'd0;
         end
     endtask
 
+    /* Read data from address */
     task read_value;
-        input [15:0] expected_data, address;
+        input [15:0] address;
         begin
-            cpu_request = { 1'd0, 16'd0, address };
+            #period;
+            cpu_request = { 1'd0, 8'd0, address };
             cpu_request_ready = 1'd1;
             while(data_out_ready == 1'd0 && memory_request_ready == 1'd0) begin
                 #period;
             end
             // simulate memory
             if(memory_request_ready == 1'd1) begin
-                memory_response = expected_data;
+                // starting at address * 8 bits going to address * 8 + 16 bits
+                if(address[0] == 1'b0)
+                    memory_response = sim_memory[address * 8+:16];
+                // starting at (address - 1) * 8 bits going to (address - 1) * 8 + 16 bits
+                else
+                    memory_response = sim_memory[(address - 1) * 8+:16];
                 memory_response_ready = 1'd1;
                 // wait for data
                 while(data_out_ready == 1'd0) begin
@@ -83,13 +102,14 @@ module cache_tb;
                 end
             end
 
-            cpu_request = 33'd0;
+            cpu_request = 25'd0;
             cpu_request_ready = 1'd0;
             memory_response = 16'd0;
             memory_response_ready = 1'd0;
         end
     endtask
 
+    /* Makes sure that data_out is the expected 8 bit value */
     task check_data;
         input [7:0] expected;
         begin
@@ -104,12 +124,30 @@ module cache_tb;
         reset_cache();
 
         $display("Beginning tests");
-        write_value(16'd55, 16'd13);
+        write_value(8'd55, 16'd12);
         check_data(8'd55);
 
-        read_value(16'd55, 16'd13);
+        write_value(8'd56, 16'd13);
+        check_data(8'd56);
+        read_value(16'd12);
         check_data(8'd55);
-            
+
+        read_value(16'd13);
+        check_data(8'd56);
+
+        write_value(8'd34, 16'd13);
+        check_data(8'd34);
+        read_value(16'd13);
+        check_data(8'd34);
+
+        write_value(8'd21, 16'd12);
+        check_data(8'd21);
+        read_value(16'd12);
+        check_data(8'd21);
+        read_value(16'd13);
+        check_data(8'd34);
+        read_value(16'd12);
+        check_data(8'd21);
         $display("All tests passed :)");
         $stop;
     end
